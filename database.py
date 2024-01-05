07d7ab5f6a79299f1c7db9d1232e60dbc63c06a8
Emyr298 <emi.ru298@gmail.com>
2024-01-05 18:09:19 +0700

    feat: add env

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///./db.sqlite') 
+DATABASE_URL = os.environ.get('DATABASE_URL') 
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
